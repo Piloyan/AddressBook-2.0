3ae093d6886f647f7bc3b4ecfd2ad8d7c0e8bca1
User <User@USER-PC>
2022-07-22 00:56:53 +0400

    Add project files.

---
+++ AddressBook 2.0/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook_2._0.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ AddressBook 2.0/Models/PersonDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook_2._0.Models
+{
+    public class PersonDbContext:DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext>options):base(options)
+        {
+                
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
